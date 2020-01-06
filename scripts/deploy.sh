d5364d923f1a132195c00b01a6e98684f34bd598
JounQin <admin@1stg.me>
2020-01-06 22:31:52 +0800

    chore: disable npm publish on CI due to 2FA

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,5 +14,5 @@ git fetch origin v"$PKG_VERSION" || {
 	yarn global add standard-version
 	standard-version -a --release-as "$PKG_VERSION"
 	git push --follow-tags origin "$GH_BRANCH"
-	npm publish
+	# npm publish
 }
